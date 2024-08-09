f556d9991f0732fbfd007aaff6b69d4856093fbb
cryptd007 <contact.tylerli.sf@gmail.com>
2024-08-09 02:27:14 -0700

    added problem-set-0

---
+++ problem-set-0/hello-world/hello-world.py
@@ -0,0 +1,11 @@
+def main():
+    # TODO: Create a script that will prompt the user for their name and the concatenate it to some form of a "Hello World" output!
+    # TODO: (i.e "Hello World, John"). Extra kudos if you can implement input checks to ensure that the user actually entered a name and not a number or a symbol.
+
+    pass # Remove 'pass' once you begin working in this function.
+
+
+# This line of code ensures that this Python file is only executed if it's ran directly. In case if this file is imported to another file, this Python file 
+# will not run. For more info, watch this video: https://www.youtube.com/watch?v=o4XveLyI6YU&pp=ygUVaWYgX19uYW1lX18gaW4gcHl0aG9u
+if __name__ == "__main__":
+    main()
---
+++ problem-set-0/syntax-errors/syntax-errors.py
@@ -0,0 +1,22 @@
+# Import the math module to use "pi"
+import math
+
+def main():
+    """This functions calculates the volume of a cone based off user input"""
+    # TODO: Identify the syntax errors and fix them, while adding a comment on what was wrong. Once all errors
+    # TODO: are fixed, this program should run as expected. There are 3 syntax errors in total.
+
+    # Prompts the user for input and converts it into an integer (because the input() functions returns a string by default)
+    height = int(input("What is height of the cone?: ))
+    radius = int(input("What is the radius of the cone?: ")
+
+
+    # * means multiplication, ** means exponents. 
+    coneVolume == (1/3)*math.pi*(radius**2)*height
+
+    print("The volume of your cone is: ", coneVolume)
+
+
+
+if __name__ == "__main__":
+    main()
