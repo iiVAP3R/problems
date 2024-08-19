45e89db752b95f301c2b5f83deb3c1b0c393a92a
iiVAP3R <47293210+iiVAP3R@users.noreply.github.com>
2024-08-18 23:30:45 -0700

    Update input-checker.py

--- problem-set-1/input-checker.py
+++ problem-set-1/input-checker.py
@@ -1,9 +1,19 @@
 def main():
-    # TODO: Your goal is to implement a conditional that checks if a user correctly enters a number. If not, **loop indefinitely** until the user does.
-    # TODO: Then, print out the user's number with accompanying text.
-    
-    pass
+  # TODO: Your goal is to implement a conditional that checks if a user correctly enters a number. If not, **loop indefinitely** until the user does.
+  # TODO: Then, print out the user's number with accompanying text.
+
+  while True:
+    try:
+
+      ui = input("Enter a number: ")
+      num = float(ui)
+      break
+    except ValueError:
+      print("not a valid num. please enter a valid num.")
+
+  print(f"you have entered: {num}.")
+
 
 
 if __name__ == "__main__":
-    main()
+  main()
